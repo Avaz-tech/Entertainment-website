04e9f1a5c140fbc70d4aa5c9d59dbd188e2dec2f
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-09-05 14:09:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Variables: undefined, null, boolean, string, obj, symbol
 
-var name = "Avazbek"
+var owner = "Avazbek Ravshanov"
 
-console.log(name)
+console.log(owner)
+
+var text =  "The web site was built by 'Avaz-tech'"
+
+console.log(text)
+
+var text2 =  "ONLY BEST QUALITY MOVIES"
+console.log(typeof(text2))
+console.log(text2.length)
