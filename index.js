842e7d7420053576d2d97f4c8ae8a9519fc3908d
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-09-08 23:50:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Variables: undefined, null, boolean, string, obj, symbol
 
-var owner = "Avazbek Ravshanov"
+var owner = "The owner: 'Avazbek Ravshanov' "
 
 console.log(owner)
 
-var text =  "The web site was built by 'Avaz-tech'"
+var text =  "The web site was built by 'Avaz-tech' "
 
 console.log(text)
 
 var text2 =  "ONLY BEST QUALITY MOVIES"
 console.log(typeof(text2))
 console.log(text2.length)
+
+var arr = ["Marvel", "Sony Pictures", "Universal Pictures"]
+
+arr.push('20th Century')
+
+console.log(arr)
+
