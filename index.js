6ee129fb22b752b4336dca0091fb937ab453975b
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-09-13 01:05:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var text =  "The web site was built by 'Avaz-tech' "
 
 console.log(text)
 
-var text2 =  "ONLY BEST QUALITY MOVIES"
-console.log(typeof(text2))
-console.log(text2.length)
+
+//console.log(typeof(text2))
+//onsole.log(text2.length)
 
 var arr = ["Marvel", "Sony Pictures", "Universal Pictures"]
 
@@ -18,3 +18,18 @@ arr.push('20th Century')
 
 console.log(arr)
 
+function kino(txt, thnk){
+    console.log("Ko'ramiz\n" + txt  + thnk);
+
+}
+
+kino("ONLY BEST QUALITY MOVIES" ,"\nThankyou")
+
+
+function sum(x,y){
+
+    console.log("Natija: " + (x*y));
+
+}
+
+sum(3,4)
