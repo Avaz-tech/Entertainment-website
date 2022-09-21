316950a30bd7d7d084d4685392f1989b1ce27c4a
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-09-21 10:37:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+
 arrows = document.querySelectorAll(".arrow");
+movieLists = document.querySelectorAll(".movie-list");
 
 console.log(arrows)
 
 arrows.forEach((arrow,i) => {
     arrow.addEventListener('click',() =>{
-        console.log("you clicked to " + i)
-    })
+      movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value -300}px)`;
+    });
 });
