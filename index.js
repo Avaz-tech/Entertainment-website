bc34336a1fafdc7e18e9ddb77ec726e840f75fb5
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-09-21 10:17:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,9 @@
-// Variables: undefined, null, boolean, string, obj, symbol
+arrows = document.querySelectorAll(".arrow");
 
-var owner = "The owner: 'Avazbek Ravshanov' "
+console.log(arrows)
 
-console.log(owner)
-
-var text =  "The web site was built by 'Avaz-tech' "
-
-console.log(text)
-
-
-//console.log(typeof(text2))
-//onsole.log(text2.length)
-
-var arr = ["Marvel", "Sony Pictures", "Universal Pictures"]
-
-arr.push('20th Century')
-
-console.log(arr)
-
-function kino(txt, thnk){
-    console.log("Ko'ramiz\n" + txt  + thnk);
-
-}
-
-kino("ONLY BEST QUALITY MOVIES" ,"\nThankyou")
-
-
-function sum(x,y){
-
-    console.log("Natija: " + (x*y));
-
-}
-
-sum(3,4)
-
-
-
- var num = 3.1
-
- console.log(Math.round(num))
-
- console.log(Math.pow(4,5))
- console.log(Math.abs(-89))
-
-var myArr = [];
-var i = 0;
-while (i<5){
-myArr.push(i);
-i++ 
-}
-console.log(myArr);
-
-let person = {
-    name: 'Ben',
-    age: 26
-};
-console.log(person);
-
-person.name = "Avaz"
-
-console.log(person);
-
-function randFrac(){
-
-    return Math.random();
-}
-
-console.log(randFrac(1,10));
-
-function conveToItn(str) {
-
- return parseInt(str);
-   
-}
-
-console.log(conveToItn("56"));  
-
-function checkEqual(a,b){
-   return a===b ? true : false;
-
-}
-
-console.log(checkEqual(5,6));
+arrows.forEach((arrow,i) => {
+    arrow.addEventListener('click',() =>{
+        console.log("you clicked to " + i)
+    })
+});
