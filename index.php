bec27a4747e8c01719604b6b0d8c4bbc5c58dd9c
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-03 20:13:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,12 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
+
+  $bad_string = "Hi! <script src='http://www.evilsite.com/bad_script.js'></script> It's a good day!";
+  $good_string = sanitize($bad_string);
+  // $good_string returns "Hi! It\'s a good day!"
+
+  // Also use for getting POST/GET variables
+  $_POST = sanitize($_POST);
+  $_GET  = sanitize($_GET);
 ?>
