41a80eabdf186683c3fdab9d347d362f6da854b3
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-04 16:48:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,12 @@ echo "Today is " . date("l");
   // Also use for getting POST/GET variables
   $_POST = sanitize($_POST);
   $_GET  = sanitize($_GET);
+
+
+$point1 = array('lat' => 40.770623, 'long' => -73.964367);
+$point2 = array('lat' => 40.758224, 'long' => -73.917404);
+$distance = getDistanceBetweenPointsNew($point1['lat'], $point1['long'], $point2['lat'], $point2['long']);
+foreach ($distance as $unit => $value) {
+    echo $unit.': '.number_format($value,4).'<br />';
+}
 ?>
