f0978ea7778d6d4cdebaf5ab6b7a3d7a6be7ebc6
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-05 23:45:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -24,13 +24,6 @@ foreach ($distance as $unit => $value) {
 }
 
 
-$expected=array('username','age','city','street');
-foreach($expected as $key){
-    if(!empty($_POST[$key])){
-        ${key}=$_POST[$key];
-    }
-    else{
-        ${key}=NULL;
-    }
+
 }
 ?>
