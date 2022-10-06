7d3796bc09f865ca83fb6813df5d23ac83cc4a12
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-06 15:45:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -30,4 +30,21 @@ echo get_resource_type($doc->doc) . "\n";
 
 
 }
+
+<?php
+	// Switch Statement Example
+	switch ($i) {
+    	case "free":
+    	    echo "i is free";
+    	    break;
+    	case "code":
+    	    echo "i is code";
+    	    break;
+    	case "camp":
+    	    echo "i is camp";
+    	    break;
+    	default:
+    	    echo "i is freecodecamp";
+            break;
+	}
 ?>
