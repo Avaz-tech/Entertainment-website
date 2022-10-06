0202eb2873b18dc57a4cba582dea4c6e951e4a95
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-06 15:44:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,18 @@ echo "Today is " . date("l");
   $_POST = sanitize($_POST);
   $_GET  = sanitize($_GET)
 
+    <?php
+// prints: mysql link
+$c = mysql_connect();
+echo get_resource_type($c) . "\n";
+
+// prints: stream
+$fp = fopen("foo", "w");
+echo get_resource_type($fp) . "\n";
+
+// prints: domxml document
+$doc = new_xmldoc("1.0");
+echo get_resource_type($doc->doc) . "\n";
 
 
 }
