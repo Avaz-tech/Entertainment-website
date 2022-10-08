3be8f6ed788dcd685e40d14a8fcb869f3709a5dc
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-08 16:51:44 +0900

    Update react.js

--- react.js
+++ react.js
@@ -17,3 +17,11 @@ class Cat extends React.Component {
       );
     }
   }
+const Cat = props => {
+  return (  
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.color}</p>
+    </div>;
+  );
+};
