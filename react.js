3a06f0230010242bf7fb61a9aaae17d85068bf46
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-11 17:48:46 +0900

    Update react.js

--- react.js
+++ react.js
@@ -25,17 +25,4 @@ const Cat = props => {
     </div>;
   );
 };
-import React from 'react';
 
-class MyComponent extends React.Component {
-  render(){
-    return (
-      <React.Fragment>
-        <div>I am an element!</div>
-        <button>I am another element</button>
-      </React.Fragment>
-    );
-  }
-}
-
-export default MyComponent;
