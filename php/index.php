b1ed008c63f8369189e7182a00d90c3ec9fb53e9
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-14 04:24:46 +0900

    Update index.php

--- php/index.php
+++ php/index.php
@@ -17,6 +17,21 @@ echo $newone;
    echo "<br>" ;
    echo (max(5,3.5,-56,50,345));
 
+echo(rand(10,50));
+
+define("companies",["Google", "Facebook", "Twitter","Insta"]);
+echo "<br>";
+echo companies[1];
+echo "<br>";
+
+$raq = array(1,4,2.7,2,6,4,0,78,45,34,32);
+sort($raq);
+
+for($i=0; $i<count($raq); $i++)
+{
+  echo $raq[$i]. " ";
+}
+
 
 ?>
 
