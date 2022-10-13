7fb182666f8939925f8da057bb4dec3075752bd5
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-13 17:57:59 +0900

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,6 +7,21 @@ echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 
+class Car{
+
+  public $model;
+  public $price;
+
+  public function __construct($model,$price){
+      $this ->model = $model;
+      $this ->price = $price;
+
+  }
+  public function txt(){
+    return "This is " .$this->model .  "and it's price is " . $this->price . "!";
+  }
+ }
+
 
 
 ?>
