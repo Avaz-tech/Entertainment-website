ef1d7b1519c9ab2675686d9b16b5f91e8d1b691b
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-15 00:05:12 +0900

    Update index.php

--- php/index.php
+++ php/index.php
@@ -8,36 +8,6 @@ echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 
 
-define("companies",["Google", "Facebook", "Twitter","Insta"]);
-echo "<br>";
-echo companies[1];
-echo "<br>";
-
-$raq = array(1,4,2.7,2,6,4,0,78,45,34,32);
-sort($raq);
-
-for($i=0; $i<count($raq); $i++)
-{
-  echo $raq[$i]. " ";
-}
-function unique_names($array1, $array2) 
-{
-    for($i=0; $i<count($array1); $i++)
-    {
-       for($k=0; $k<count($array2); $k++)
-       {
-         if($array1[$i]==$array2[$k])
-         {
-             $array1[$i]=NULL;
-         }
-       } 
-    }
-    return join($array1,$array2);
-    
-}
-
- $names = unique_names(['Ava', 'Emma', 'Olivia'], ['Olivia', 'Sophia', 'Emma']);
-echo join(', ', array($names)); // should print Emma, Olivia, Ava, Sophia
 class TextInput
 {
     public $result='';
