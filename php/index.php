526933f26181ee28442d16b48d89f50c929adb0a
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-15 00:04:24 +0900

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,17 +7,6 @@ echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 
-$c = 56.78;
-$newone = (int)$c;
-echo $newone;
-
-
-   echo "<br>" ;
-   echo (min(5,3.5,-56,50,345));
-   echo "<br>" ;
-   echo (max(5,3.5,-56,50,345));
-
-echo(rand(10,50));
 
 define("companies",["Google", "Facebook", "Twitter","Insta"]);
 echo "<br>";
