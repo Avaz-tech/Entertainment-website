4f097e7551d26a83a143496dcb98b92c925d1518
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-10-20 04:18:48 +0900

    Update index.php

--- php/index.php
+++ php/index.php
@@ -8,20 +8,7 @@ echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 
 
-class TextInput
-{
-    public $result='';
 
-    public function add($input)
-    {
-     $this->result= $this->result.$input;
-    }
-
-    public function getValue()
-    {
-      return $this->result;
-    }
-}
 
 
 ?>
