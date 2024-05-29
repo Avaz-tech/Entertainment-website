bf5d859e1814ac727a54dfbac7492a6f125c9758
Ben96 <avazravshan0@gmail.com>
2024-05-29 18:10:32 +0900

    header (navbar): page scroll event added

--- src/js/nav.js
+++ src/js/nav.js
@@ -155,4 +155,13 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileNavBackdropInstance.classList.remove("visible");
     mobileNavContainerInstance.classList.add("mobile-nav-closed");
   });
+
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
 });
